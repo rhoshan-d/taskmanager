ee4606199c79442ac66d9e99f6f9fe2d13b841c3
Rhoshan <rhoshan02@gmail.com>
2024-09-13 00:17:30 +0100

    Half complete. never did I think loading a modal would be so hard :)

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize sidenav
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // Initialize datepicker
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // Initialize select elements
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // Initialize Modals
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);  // Modal initialization
+
+});
